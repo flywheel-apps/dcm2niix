2125c8c6dffb73ed922a0db1903c8b670439214d
Margaret Mahan <margaret.y.mahan@gmail.com>
2020-11-10 14:56:24 -0600

    MAIN: Update instance tests accordingly.

--- tests/instance/assess_instance_runs.py
+++ tests/instance/assess_instance_runs.py
@@ -1,25 +1,26 @@
+import datetime
+
 import flywheel
 import pandas as pd
 
+versions = ["1.0.0_1.0.20200331", "1.1.0_1.0.20201102_dev0"]
+
 
 input_dir = "~/Documents/flywheel/gears/dcm2niix/tests/instance"
+date = str(datetime.date.today())
 
-original = pd.read_csv(
-    f"{input_dir}/dcm2niix_instance_runs_0.9.0_1.0.20190902.rc2_2020-09-30.csv"
-)
-rewrite = pd.read_csv(
-    f"{input_dir}/dcm2niix_rewrite_test_1.0.0_1.0.20200331_2020-09-30.csv"
-)
+previous = pd.read_csv(f"{input_dir}/dcm2niix_instance_runs_{version[0]}_{date}.csv")
+updated = pd.read_csv(f"{input_dir}/dcm2niix_instance_runs_{version[1]}_{date}.csv")
 
 # Remove where inputs, outputs, and job status are equivalent
-equivalent = original.merge(
+equivalent = previous.merge(
     rewrite, how="inner", on=["inputs", "destination_id", "state", "outputs"]
 )
-original = original.loc[~original["job_id"].isin(equivalent["job_id_x"].to_list())]
-rewrite = rewrite.loc[~rewrite["job_id"].isin(equivalent["job_id_y"].to_list())]
+previous = previous.loc[~previous["job_id"].isin(equivalent["job_id_x"].to_list())]
+updated = updated.loc[~updated["job_id"].isin(equivalent["job_id_y"].to_list())]
 
 
-compare = original.merge(rewrite, how="left", on=["inputs", "destination_id"])
+compare = previous.merge(updated, how="left", on=["inputs", "destination_id"])
 compare = compare[
     ["job_id_x", "job_id_y", "state_x", "state_y", "outputs_x", "outputs_y"]
 ]
